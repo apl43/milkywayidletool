1b2d936de817a694846631b8fc2b2b3b13e10e86
apl43 <1397381851@qq.com>
2025-12-21 16:41:53 +0800

    fix

--- tool/AlchemyAssistant.go
+++ tool/AlchemyAssistant.go
@@ -10,18 +10,7 @@ import (
 
 // }
 
-// // 计算转化产出
-// func CalculateTheTransformationOutput(i *models.AlchemyTransformationConfig) {
-// 	i.Cost = models.AlchemyCost{ItemID: 1, Quantity: 10, GoldCost: 100, CatalystID: 0}
-// 	fmt.Println("111", i.Cost)
-// }
-
-
-func Te() {
-	fmt.Println("测试")
-	var a models.AlchemyTransformationConfig
-	a = models.AlchemyTransformationConfig{
-		Cost: models.AlchemyCost{ItemID: 1, Quantity: 10, GoldCost: 100, CatalystID: 0},
-	}
-	fmt.Println(a)
+// 计算转化产出
+func CalculateTheTransformationOutput(i *models.AlchemyTransformationConfig) {
+	fmt.Printf("input: %v\n", i)
 }
