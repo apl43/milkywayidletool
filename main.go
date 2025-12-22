db8c43238acc7e4492adb717270bb6488d48bff1
apl43 <1397381851@qq.com>
2025-12-23 00:29:17 +0800

    clear

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"fmt"
-	"milkywayidletool/tool"
 )
 
 func main() {
-	// cfg := models.AlchemyTransformationConfig{}
-	// tool.CalculateTheTransformationOutput(&cfg)
-	fmt.Println("111")
-	tool.Te()
+	fmt.Println("Hello, Milky Way Idle Tool!")
 }
