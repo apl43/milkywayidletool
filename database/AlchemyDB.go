886354f949823e25b0a352fe08d14fdbacb4fc1b
apl43 <1397381851@qq.com>
2025-12-28 21:26:17 +0800

    update

--- database/AlchemyDB.go
+++ database/AlchemyDB.go
@@ -46,4 +46,204 @@ var ConversionRecipeMap = map[string]models.ConversionRecipe{
 		},
 		RecommendedLevel: 50,
 	},
+	"复活": {
+		Name:        "复活",
+		Quantity:    1,
+		GoldCost:    2000, 
+		ActionTime:  20,
+		SuccessRate: 0.5,
+		MainProducts: []models.ProductProbability{
+			{Name: "复活", Quantity: 1, Chance: 0.125},
+			{Name: "疯狂", Quantity: 1, Chance: 0.125},
+			{Name: "无敌", Quantity: 1, Chance: 0.125},
+			{Name: "速度光环", Quantity: 1, Chance: 0.125},
+			{Name: "守护光环", Quantity: 1, Chance: 0.125},
+			{Name: "物理光环", Quantity: 1, Chance: 0.125},
+			{Name: "暴击光环", Quantity: 1, Chance: 0.125},
+			{Name: "元素光环", Quantity: 1, Chance: 0.125},
+		},
+		ExtraOutput: models.ExtraOutput{
+			EssenceName:    "炼金精华",
+			EssenceChance:  0, // 待修改
+			RareDropName:   "中工匠匣",
+			RareDropChance: 0, // 待修改
+			ExperienceGain: 0, // 待修改
+		},
+		RecommendedLevel: 60,
+	},
+	"疯狂": {
+		Name:        "疯狂",
+		Quantity:    1,
+		GoldCost:    2000, 
+		ActionTime:  20,
+		SuccessRate: 0.5,
+		MainProducts: []models.ProductProbability{
+			{Name: "复活", Quantity: 1, Chance: 0.125},
+			{Name: "疯狂", Quantity: 1, Chance: 0.125},
+			{Name: "无敌", Quantity: 1, Chance: 0.125},
+			{Name: "速度光环", Quantity: 1, Chance: 0.125},
+			{Name: "守护光环", Quantity: 1, Chance: 0.125},
+			{Name: "物理光环", Quantity: 1, Chance: 0.125},
+			{Name: "暴击光环", Quantity: 1, Chance: 0.125},
+			{Name: "元素光环", Quantity: 1, Chance: 0.125},
+		},
+		ExtraOutput: models.ExtraOutput{
+			EssenceName:    "炼金精华",
+			EssenceChance:  0, // 待修改
+			RareDropName:   "中工匠匣",
+			RareDropChance: 0, // 待修改
+			ExperienceGain: 0, // 待修改
+		},
+		RecommendedLevel: 60,
+	},
+	"无敌": {
+		Name:        "无敌",
+		Quantity:    1,
+		GoldCost:    2000, 
+		ActionTime:  20,
+		SuccessRate: 0.5,
+		MainProducts: []models.ProductProbability{
+			{Name: "复活", Quantity: 1, Chance: 0.125},
+			{Name: "疯狂", Quantity: 1, Chance: 0.125},
+			{Name: "无敌", Quantity: 1, Chance: 0.125},
+			{Name: "速度光环", Quantity: 1, Chance: 0.125},
+			{Name: "守护光环", Quantity: 1, Chance: 0.125},
+			{Name: "物理光环", Quantity: 1, Chance: 0.125},
+			{Name: "暴击光环", Quantity: 1, Chance: 0.125},
+			{Name: "元素光环", Quantity: 1, Chance: 0.125},
+		},
+		ExtraOutput: models.ExtraOutput{
+			EssenceName:    "炼金精华",
+			EssenceChance:  0, // 待修改
+			RareDropName:   "中工匠匣",
+			RareDropChance: 0, // 待修改
+			ExperienceGain: 0, // 待修改
+		},
+		RecommendedLevel: 60,
+	},
+	"速度光环": {
+		Name:        "速度光环",
+		Quantity:    1,
+		GoldCost:    2000, 
+		ActionTime:  20,
+		SuccessRate: 0.5,
+		MainProducts: []models.ProductProbability{
+			{Name: "复活", Quantity: 1, Chance: 0.125},
+			{Name: "疯狂", Quantity: 1, Chance: 0.125},
+			{Name: "无敌", Quantity: 1, Chance: 0.125},
+			{Name: "速度光环", Quantity: 1, Chance: 0.125},
+			{Name: "守护光环", Quantity: 1, Chance: 0.125},
+			{Name: "物理光环", Quantity: 1, Chance: 0.125},
+			{Name: "暴击光环", Quantity: 1, Chance: 0.125},
+			{Name: "元素光环", Quantity: 1, Chance: 0.125},
+		},
+		ExtraOutput: models.ExtraOutput{
+			EssenceName:    "炼金精华",
+			EssenceChance:  0, // 待修改
+			RareDropName:   "中工匠匣",
+			RareDropChance: 0, // 待修改
+			ExperienceGain: 0, // 待修改
+		},
+		RecommendedLevel: 40,
+	},
+	"守护光环": {
+		Name:        "守护光环",
+		Quantity:    1,
+		GoldCost:    2000, 
+		ActionTime:  20,
+		SuccessRate: 0.5,
+		MainProducts: []models.ProductProbability{
+			{Name: "复活", Quantity: 1, Chance: 0.125},
+			{Name: "疯狂", Quantity: 1, Chance: 0.125},
+			{Name: "无敌", Quantity: 1, Chance: 0.125},
+			{Name: "速度光环", Quantity: 1, Chance: 0.125},
+			{Name: "守护光环", Quantity: 1, Chance: 0.125},
+			{Name: "物理光环", Quantity: 1, Chance: 0.125},
+			{Name: "暴击光环", Quantity: 1, Chance: 0.125},
+			{Name: "元素光环", Quantity: 1, Chance: 0.125},
+		},
+		ExtraOutput: models.ExtraOutput{
+			EssenceName:    "炼金精华",
+			EssenceChance:  0, // 待修改
+			RareDropName:   "中工匠匣",
+			RareDropChance: 0, // 待修改
+			ExperienceGain: 0, // 待修改
+		},
+		RecommendedLevel: 40,
+	},
+	"物理光环": {
+		Name:        "物理光环",
+		Quantity:    1,
+		GoldCost:    2000, 
+		ActionTime:  20,
+		SuccessRate: 0.5,
+		MainProducts: []models.ProductProbability{
+			{Name: "复活", Quantity: 1, Chance: 0.125},
+			{Name: "疯狂", Quantity: 1, Chance: 0.125},
+			{Name: "无敌", Quantity: 1, Chance: 0.125},
+			{Name: "速度光环", Quantity: 1, Chance: 0.125},
+			{Name: "守护光环", Quantity: 1, Chance: 0.125},
+			{Name: "物理光环", Quantity: 1, Chance: 0.125},
+			{Name: "暴击光环", Quantity: 1, Chance: 0.125},
+			{Name: "元素光环", Quantity: 1, Chance: 0.125},
+		},
+		ExtraOutput: models.ExtraOutput{
+			EssenceName:    "炼金精华",
+			EssenceChance:  0, // 待修改
+			RareDropName:   "中工匠匣",
+			RareDropChance: 0, // 待修改
+			ExperienceGain: 0, // 待修改
+		},
+		RecommendedLevel: 40,
+	},
+	"暴击光环": {
+		Name:        "暴击光环",
+		Quantity:    1,
+		GoldCost:    2000, 
+		ActionTime:  20,
+		SuccessRate: 0.5,
+		MainProducts: []models.ProductProbability{
+			{Name: "复活", Quantity: 1, Chance: 0.125},
+			{Name: "疯狂", Quantity: 1, Chance: 0.125},
+			{Name: "无敌", Quantity: 1, Chance: 0.125},
+			{Name: "速度光环", Quantity: 1, Chance: 0.125},
+			{Name: "守护光环", Quantity: 1, Chance: 0.125},
+			{Name: "物理光环", Quantity: 1, Chance: 0.125},
+			{Name: "暴击光环", Quantity: 1, Chance: 0.125},
+			{Name: "元素光环", Quantity: 1, Chance: 0.125},
+		},
+		ExtraOutput: models.ExtraOutput{
+			EssenceName:    "炼金精华",
+			EssenceChance:  0, // 待修改
+			RareDropName:   "中工匠匣",
+			RareDropChance: 0, // 待修改
+			ExperienceGain: 0, // 待修改
+		},
+		RecommendedLevel: 40,
+	},
+	"元素光环": {
+		Name:        "元素光环",
+		Quantity:    1,
+		GoldCost:    2000, 
+		ActionTime:  20,
+		SuccessRate: 0.5,
+		MainProducts: []models.ProductProbability{
+			{Name: "复活", Quantity: 1, Chance: 0.125},
+			{Name: "疯狂", Quantity: 1, Chance: 0.125},
+			{Name: "无敌", Quantity: 1, Chance: 0.125},
+			{Name: "速度光环", Quantity: 1, Chance: 0.125},
+			{Name: "守护光环", Quantity: 1, Chance: 0.125},
+			{Name: "物理光环", Quantity: 1, Chance: 0.125},
+			{Name: "暴击光环", Quantity: 1, Chance: 0.125},
+			{Name: "元素光环", Quantity: 1, Chance: 0.125},
+		},
+		ExtraOutput: models.ExtraOutput{
+			EssenceName:    "炼金精华",
+			EssenceChance:  0, // 待修改
+			RareDropName:   "中工匠匣",
+			RareDropChance: 0, // 待修改
+			ExperienceGain: 0, // 待修改
+		},
+		RecommendedLevel: 40,
+	},
 }
--- main.go
+++ main.go
@@ -2,25 +2,26 @@ package main
 
 import (
 	"fmt"
-	"milkywayidletool/database/market"
-	// "milkywayidletool/tool"
+	// "milkywayidletool/database/market"
+	"milkywayidletool/tool"
 )
 
 func main() {
 	fmt.Println("Hello, Milky Way Idle Tool!")
 	fmt.Println()
-	// tool.CalculateAllRevenue()
+	tool.CalculateAllRevenue(10)
+
 
 	// // 获取市场数据
 	// market.GetMarketData()
 
-	// 加载最新市场文件
-	latestFile, err := market.LoadLatestMarketData()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("保存时间: %s\n", latestFile.SaveTime)
-	fmt.Printf("数据时间戳: %d\n", latestFile.DataTimestamp)
+	// // 加载最新市场文件
+	// latestFile, err := market.LoadLatestMarketData()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Printf("保存时间: %s\n", latestFile.SaveTime)
+	// fmt.Printf("数据时间戳: %d\n", latestFile.DataTimestamp)
 	// market.PrintAllItems(&latestFile.MarketItems)
 }
